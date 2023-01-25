6554d045d85416d3777e6e2f0940f1a35eda1a71
EVILBEAN-cmd <evilbeancmd@gmail.com>
2023-01-25 13:03:07 +0100

    Added 0226 invert binary tree

---
+++ 0226_invert_binary_tree.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root):
+        if not root:
+            return None
+        
+        tmp = root.left
+        root.left = root.right
+        root.right = tmp
+
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+
+        return root
+
+
+"""
+Input: root of binary tree
+Output: root of inverted binary tree
+
+Recursively invert the left and right subtrees of each node.
+
+Time complexity: O(n)
+Space complexity: O(n)
+"""
