2eb36c085af5ae06681f033c996419608242cdc2
EVILBEAN-cmd <evilbeancmd@gmail.com>
2023-02-25 18:41:24 +0100

    Updated 0121

--- 0121_best_time_to_buy_and_sell_stock.py
+++ 0121_best_time_to_buy_and_sell_stock.py
@@ -1,34 +1,23 @@
 class Solution:
     def maxProfit(self, prices: list[int]) -> int:
-        max_profit = 0
-        left, right = 0, 1
-        
-        while right < len(prices):            
+        low, res = prices[0], 0
+        for cur in prices:
+            if cur < low:
+                low = cur
+            else:
+                res = max(res, cur-low)
+        return res 
             
-            current_profit = prices[right]-prices[left]
-            max_profit = max(max_profit, current_profit)
-            
-            if prices[right] < prices[left]:
-                left = right
-            
-            right += 1
-            
-        return max_profit
-
-
 """
 Input: array of integers
 Output: integer
 
-First we initialize the max_profit to 0 and the left and right pointers to 0 and 1.
-
-We loop through the array with the right pointer.
-We calculate the current profit by subtracting the left pointer from the right pointer.
-We update the max_profit if the current profit is greater than the current max_profit.
-
-If the prices[right] is less than prices[left], we update the left pointer to be the right pointer.
-Since the left pointer should always be the index of the lowest price we have seen so far. 
-In future comparisons, we want to buy at the new lowest price.
+First we initialize the lowest value and the result variable.
+We loop through the array.
+If the current value is less than the lowest value, we update the lowest value.
+Otherwise, we update the result variable by comparing the result
+with the current value minus the lowest value.
+we return the result variable.
 
 Time Complexity: O(n)
 Space Complexity: O(1)
