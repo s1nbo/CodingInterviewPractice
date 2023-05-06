8e5af05f46195dd2a520bfb7cb9103a1122eb847
EVILBEAN-cmd <evilbeancmd@gmail.com>
2023-05-07 00:46:09 +0200

    Added 1498

---
+++ 1498_number_of_subsequences_that_satisfy_the_given_sum_condition.py
@@ -0,0 +1,37 @@
+class Solution:
+    def numSubseq(self, nums, target: int) -> int:
+        nums.sort()
+        result = 0
+        mod = 10 ** 9 + 7
+        right = len(nums) - 1
+        
+        for left in range(len(nums)):
+            
+            while nums[right] +  nums[left] > target and left <= right:
+                right -= 1
+            
+            if left <= right:
+                result += 2 ** (right-left)
+
+        return result % mod
+
+
+"""
+Input: list of integers, integer
+Output: integer
+
+We sort the list of integers since we always look for min and max during the process.
+We initialize result, mod and a right pointer.
+
+We iterate through the list of integers with a left pointer.
+If the sum of the left and right pointer is greater than the target, we decrement the right pointer. (We are currently to big)
+
+If the left pointer is less than or equal to the right pointer, we add 2 ** (right-left) to the result. 
+(We are currently in the range (min = left, max = right and min + max <= target))
+
+We return the result % mod.
+ 
+Time Complexity: O(nlogn) due to sorting
+Space Complexity: O(1)
+
+"""
