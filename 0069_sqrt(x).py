ebda2021143faa42ef739a51530c81461ecfb774
EVILBEAN-cmd <evilbeancmd@gmail.com>
2023-07-18 22:00:06 +0200

    Changed 0069_sqrt(x).py

--- 0069_sqrt(x).py
+++ 0069_sqrt(x).py
@@ -6,9 +6,9 @@ class Solution:
         l, r = 1, x
         while l <= r:
             m = (l+r) // 2
-            if m == x // m:
+            if m*m == x:
                 return m
-            elif m > x // m:
+            elif m*m > x:
                 r = m-1
             else:
                 l = m+1
