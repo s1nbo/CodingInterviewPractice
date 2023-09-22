8799689c207530f6983abbaf9463410742ca3d1e
EVILBEAN-cmd <evilbeancmd@gmail.com>
2023-09-22 19:56:16 +0200

    Added 1337.py

---
+++ Python/1337.py
@@ -0,0 +1,8 @@
+import heapq
+
+class Solution:
+    def kWeakestRows(self, mat: list[list[int]], k: int) -> list[int]:
+        min_heap = []
+        for idx, row in enumerate(mat):
+            heapq.heappush(min_heap, (sum(row), idx))
+        return[heapq.heappop(min_heap)[1] for _ in range(k)]
