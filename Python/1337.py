efc4b311fe2dbdf77b9e9f95ab9d615a0d332e15
EVILBEAN-cmd <evilbeancmd@gmail.com>
2023-10-05 15:38:42 +0200

    Added Complexity Analysis

--- Python/1337.py
+++ Python/1337.py
@@ -6,3 +6,6 @@ class Solution:
         for idx, row in enumerate(mat):
             heapq.heappush(min_heap, (sum(row), idx))
         return[heapq.heappop(min_heap)[1] for _ in range(k)]
+
+# Time Complexity: O(nlogn)
+# Space Complexity: O(n)
