2b2fd69ee1616bcfee9ea81cd2b9462856adba36
EVILBEAN-cmd <evilbeancmd@gmail.com>
2023-10-06 17:21:19 +0200

    Add 0343.py

---
+++ Python/0343.py
@@ -0,0 +1,15 @@
+class Solution:
+    def integerBreak(self, n: int) -> int:
+        if n < 4:
+            return n-1
+        exp = n//3
+        match n%3:
+            case 0:
+                return 3 ** exp
+            case 1:
+                return 3 ** (exp-1) * 4
+            case 2:
+                return 3 ** exp * 2
+
+# Time Complexity: O(1)
+# Space Complexity: O(1)
