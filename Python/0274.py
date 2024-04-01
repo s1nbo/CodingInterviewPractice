10c208a1d5e905c9319df7bd6c18f4aa3c00f049
s1nbo <evilbeancmd@gmail.com>
2024-04-01 12:55:59 +0200

    Added 0274.py

---
+++ Python/0274.py
@@ -0,0 +1,13 @@
+class Solution:
+    def hIndex(self, citations: list[int]) -> int:
+        citations.sort()
+        n = len(citations)
+        for i, v in enumerate(citations):
+            if n - i <= v:
+                return n - i
+
+        return 0
+
+# Topic: Array
+# Time Complexity: O(n)
+# Space Complexity: O(1)
