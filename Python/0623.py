2ea6a228c6c311e8c9deeb1aca1a4d5ab34d630b
s1nbo <evilbeancmd@gmail.com>
2024-04-17 00:34:40 +0200

    Added 0623

---
+++ Python/0623.py
@@ -0,0 +1,35 @@
+from typing import Optional
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+
+    def dfs(self, root, val, depth):
+        if root == None:
+            return
+        if depth > 2:
+            self.dfs(root.left, val, depth-1)
+            self.dfs(root.right, val, depth-1)
+        else:
+            temp = root.left
+            root.left = TreeNode(val, temp, None)
+
+            temp = root.right
+            root.right = TreeNode(val, None, temp)
+
+
+
+    def addOneRow(self, root: Optional[TreeNode], val: int, depth: int) -> Optional[TreeNode]:
+        
+        if depth == 1:
+            temp = TreeNode(val, root, None)
+            return temp
+
+        self.dfs(root, val, depth)
+
+        return root
