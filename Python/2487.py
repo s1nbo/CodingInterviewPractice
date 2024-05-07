df211f03b2787555b7578fa5bcce1599c019bb31
s1nbo <evilbeancmd@gmail.com>
2024-05-07 23:58:57 +0200

    Test

--- Python/2487.py
+++ Python/2487.py
@@ -1,1 +1,20 @@
-#Test
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return head
+        head.next = self.removeNodes(head.next)
+        if head.next and head.val < head.next.val:
+            return head.next
+        return head
+    
+# Topic: Linked list
+# Time Complexity: O(n)
+# Space Complexity: O(n) 
