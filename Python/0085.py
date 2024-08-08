a2b7f6e8a9f9b67b92ec8c43974cbf138d2f46de
s1nbo <evilbeancmd@gmail.com>
2024-08-08 23:16:00 +0200

    Added 0085

---
+++ Python/0085.py
@@ -0,0 +1,23 @@
+class Solution:
+    def spiralMatrixIII(self, rows: int, cols: int, rStart: int, cStart: int) -> List[List[int]]:
+        direction = [[0,1], [1,0], [0, -1], [-1,0]]
+
+        ans = []
+        r, c = rStart, cStart
+        steps = 1 # Stepsize
+        i = 0 # Direction
+        while(len(ans) < rows*cols):
+            for _ in range(2):
+                dr, dc = direction[i]
+                for _ in range(steps):
+                    if (0 <= r < rows) and (0 <= c < cols):
+                        ans.append([r, c])
+                    r, c = + r + dr, c + dc
+                i = (i+1)%4
+            steps += 1
+        
+        return ans
+
+# Topic: Array, Math
+# Time Complexity: O(rows*cols)
+# Space Complexity: O(rows*cols)
