abea7fe94bc977bbbe9447b2513007aa2fcd8014
s1nbo <evilbeancmd@gmail.com>
2024-08-13 00:07:09 +0200

    Added 0703

---
+++ Python/0703.py
@@ -0,0 +1,26 @@
+from typing import List
+import heapq
+
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.nums = nums
+        self.k = k
+        heapq.heapify(self.nums)
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.nums, val)
+
+        while len(self.nums) > self.k:
+            heapq.heappop(self.nums)
+        return self.nums[0]
+
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
+
+
+# Topic: Heap
+# Time Complexity: O(n log k)
+# Space Complexity: O(k)
