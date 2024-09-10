b72b0399000f351cc1b32822303803fc94e5cac2
s1nbo <evilbeancmd@gmail.com>
2024-09-10 15:53:04 +0200

    own GCD using Euclidean algorithm

--- Python/2807.py
+++ Python/2807.py
@@ -7,13 +7,18 @@ class ListNode:
         self.next = next
 class Solution:
     def insertGreatestCommonDivisors(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        def GCD(a, b):
+            while b != 0: 
+                a, b = b, a % b
+            return a
+
         if head.next == None:
             return head
         prev = head
         cur = head.next
 
         while cur:
-            node_value = math.gcd(prev.val, cur.val)
+            node_value = GCD(prev.val, cur.val)
             gcd_node = ListNode(val=node_value, next=cur)
             prev.next = gcd_node
             
