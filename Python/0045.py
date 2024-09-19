e7479139028795fa6752eb75531908cee7d568c3
s1nbo <evilbeancmd@gmail.com>
2024-09-19 21:01:55 +0200

    Changed 0045

--- Python/0045.py
+++ Python/0045.py
@@ -15,27 +15,6 @@ class Solution:
         return counter
 
 
-"""
-Input: list of integers
-Output: integer
-
-We create a counter, left, and right.
-
-We iterate while right is less than the length of the list minus 1.
-We create a variable to store the farthest jump.
-We iterate through the list from left to right plus 1.
-We update farthest_jump to the maximum of farthest_jump and i plus nums[i].
-
-We update left to right plus 1.
-We update right to farthest_jump.
-We increment counter.
-
-We return counter.
-
-This solution is a greedy algorithm. We iterate through the list and keep track of the farthest jump we can make.
-We update left and right to the next range of jumps we can make. We increment counter.
-left and right are the range of jumps we can make. We iterate through the list from left to right plus 1.
-
-Time Complexity: O(n)
-Space Complexity: O(1)
-"""
+# Topics: Array, Greedy
+# Time Complexity: O(n)
+# Space Complexity: O(1)
