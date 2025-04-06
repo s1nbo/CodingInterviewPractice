da2daaa2d422f99da7c8387c7783e7508df61f0f
s1nbo <evilbeancmd@gmail.com>
2025-04-06 23:35:45 +0200

    added comments

--- C++/0368.cpp
+++ C++/0368.cpp
@@ -11,14 +11,17 @@ class Solution {
             for (int i = 0; i < nums.size(); ++i) {
                 for (int j = 0; j < i; ++j) {
                     if (nums[i] % nums[j] == 0 && dp[i] < dp[j] + 1) {
+                        // Update size of the subset and previous index
                         dp[i] = dp[j] + 1;
                         prev[i] = j;
                     }
                 }
+                // Update maxIndex if we found a larger subset
                 if (dp[i] > dp[maxIndex]) {
                     maxIndex = i;
                 }
             }
+            // Build the result subset by following the prev array
             std::vector<int> result;
             while (maxIndex != -1) {
                 result.push_back(nums[maxIndex]);
